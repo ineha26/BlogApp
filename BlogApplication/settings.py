df98b41410aa1ef478582559d7c435dfb7d214f6
ineha26 <43500311+ineha26@users.noreply.github.com>
2021-05-19 17:38:19 +0500

    Update settings.py

--- BlogApplication/settings.py
+++ BlogApplication/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'diaimran4@gmail.com'
+EMAIL_HOST_USER = '*******'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = True
